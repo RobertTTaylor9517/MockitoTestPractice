e0e68b753206a9776d3dea6092adece32dc03fc9
robert <robt15@hotmail.com>
2020-05-29 17:22:24 -0400

    Hamcrest Matchers

---
+++ src/test/java/mockitocourse/HamcrestMatchersTest.java
@@ -0,0 +1,46 @@
+package mockitocourse;
+
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class HamcrestMatchersTest {
+
+	@Test
+	public void test() {
+		List<Integer> scoresIntegers = Arrays.asList(99, 100, 101, 105);
+		
+		//scores has 4 items
+		assertThat(scoresIntegers, hasSize(4));
+		assertThat(scoresIntegers, hasItems(99, 100));
+		
+		//every item: > 90
+		assertThat(scoresIntegers, everyItem(greaterThan(90)));
+		assertThat(scoresIntegers, everyItem(lessThan(190)));
+		
+		//String
+		assertThat("", isEmptyString());
+		assertThat(null, isEmptyOrNullString());
+		
+		//Arrays
+		Integer[] marks = {1,2,3};
+		
+		assertThat(marks, arrayWithSize(3));
+		assertThat(marks, arrayContaining(1,2,3));
+		
+	}
+
+}
